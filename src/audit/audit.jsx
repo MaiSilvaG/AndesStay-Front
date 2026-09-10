cb3c0308e5077943c906634520ad3a1580da3436
MaiSilvaG <mai.silvag@duocuc.cl>
2026-09-10 09:47:32 -0300

    avance de audit

--- src/audit/audit.jsx
+++ src/audit/audit.jsx
@@ -1,39 +1,64 @@
 import Table from 'react-bootstrap/Table';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
 function Audit() {
   return (
-    <div className='container mt-4'>
-      <Table bordered hover>
-        <thead>
-          <tr>
-            <th>Usuarios</th>
-            <th>Fecha</th>
-            <th>Unidad</th>
-            <th>Tipo Evento</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>@twitter</td>
-            <td>@qsq</td>
-            <td>@qsq</td>
-          </tr>
-        </tbody>
-      </Table>
+    <div className='m-5'>
+      <Row>
+          <Col sm={8}>
+            <Card>
+              <Card.Body>
+                <Table bordered hover>
+                  <thead>
+                    <tr>
+                      <th>Usuarios</th>
+                      <th>Fecha</th>
+                      <th>Unidad</th>
+                      <th>Tipo Evento</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1</td>
+                      <td>Mark</td>
+                      <td>Otto</td>
+                      <td>@mdo</td>
+                    </tr>
+                    <tr>
+                      <td>2</td>
+                      <td>Jacob</td>
+                      <td>Thornton</td>
+                      <td>@fat</td>
+                    </tr>
+                    <tr>
+                      <td>3</td>
+                      <td>@twitter</td>
+                      <td>@qsq</td>
+                      <td>@qsq</td>
+                    </tr>
+                  </tbody>
+                </Table>
+              </Card.Body>
+            </Card>
+          </Col>
+
+          <Col xs={6} md={4}>
+            <Card>
+              <Card.Body>
+                <Card.Title>Filtros</Card.Title>
+                <Card.Text>
+                  tengo que poner filtros por
+                  * Usuarios
+                  * Fecha
+                  * tipo de evento
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+      </Row>
     </div>
-    
   );
 }
 
